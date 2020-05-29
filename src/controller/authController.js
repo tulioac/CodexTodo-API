1f49ba4f33eea8f33943fc6700df5ee6cceb15e9
Tulio <tutex1500@gmail.com>
2020-05-29 11:49:35 -0300

    Autenticação do usuário pelo token

---
+++ src/controller/authController.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
+
+const Todo = require('../models/todo');
+const User = require('../models/user');
+
+const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, { expiresIn: 86400 });
+}
+
+router.post('/register', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.send({
+      user,
+      token: generateToken({ id: user.id })
+    });
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
+});
+
+router.post('/authenticate', async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).send({ error: "User not found" });
+  }
+
+  return res.send({
+    user,
+    token: generateToken({ id: user.id })
+  });
+});
+
+module.exports = app => app.use('/auth', router);
