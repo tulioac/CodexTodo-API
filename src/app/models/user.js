ed1fec5f2126ac1da5254326b6c7f984875fc468
Tulio <tutex1500@gmail.com>
2020-06-11 13:37:39 -0300

    Adição de um nome para o usuário

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,6 +2,10 @@ const mongoose = require('../../database');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true
