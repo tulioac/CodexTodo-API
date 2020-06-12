18007b98d8038aa85a1014276f2186993f1683b1
Tulio <tutex1500@gmail.com>
2020-06-12 18:16:55 -0300

    Get básico

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require('./app/controller/authController')(app);
 require('./app/controller/todoController')(app);
 
+app.get('/', (req, res) => {
+  res.send({ msg: "Ok" });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor ligado...');
 })
